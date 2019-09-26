2e0f86ded0be0548aec2209f62ad2da9e9bb1ddb
Piotr Dubiel <piotrdubiel@icloud.com>
2019-09-26 11:35:27 +0200

    Edit App component

--- src/App.js
+++ src/App.js
@@ -3,24 +3,6 @@ import { render } from 'react-dom';
 import Pet from './Pet';
 
 const App = () => {
-  // return React.createElement('div', { id: 'something-important' }, [
-  //   React.createElement('h1', {}, 'Adopt Me!'),
-  //   React.createElement(Pet, {
-  //     name: 'Pepper',
-  //     animal: 'Bird',
-  //     breed: 'Cockatiel'
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: 'Doink',
-  //     animal: 'Cat',
-  //     breed: 'Mixed'
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: 'Bolt',
-  //     animal: 'Dog',
-  //     breed: 'Mixed'
-  //   })
-  // ]);
   return (
     <div>
       <h1 id="something-important">Adopt Me</h1>
