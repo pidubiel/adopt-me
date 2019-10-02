6f60ac577332d78ad84083931e18761418a744e4
Piotr Dubiel <piotrdubiel@icloud.com>
2019-10-02 18:05:04 +0200

    Configure ESLint for hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+// import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me</h1>
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      {/* <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
       <Pet name="Doink" animal="Cat" breed="Mixed" />
-      <Pet name="Claw" animal="Cat" breed="Persian" />
+      <Pet name="Claw" animal="Cat" breed="Persian" /> */}
+      <SearchParams />
     </div>
   );
 };
